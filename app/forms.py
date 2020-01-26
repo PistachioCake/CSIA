1a78ca5bfd97c4877edaa4d4152b685ed3b68203
PistachioCake <rushil.udani@gmail.com>
2020-01-26 16:51:24 -0600

    Add a sample form at "url/forms", and a config.py
    config.py allows for easy setting of config variables in the flask application.
    app\forms.py creates the forms we can use in this application.
    app\routes.py now includes forms, which renders the forms.
    app\template\forms.html is the html source for forms.
    All links (template\base.html and app\routes.py) now use flask.url_for.
    requirements.txt has been updated.

---
+++ app/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SubmitField
+from wtforms.validators import DataRequired
+
+class SchoolForm(FlaskForm):
+    school_name = StringField('School name', validators=[DataRequired()])
+    no_of_teams = IntegerField('Number of Teams')
+    submit = SubmitField('Submit')
+    
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,16 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import SchoolForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return render_template("index.html", title="Home")
+    return render_template("index.html", title="Home")
+
+@app.route('/forms', methods=['GET', 'POST'])
+def forms():
+    form = SchoolForm()
+    if form.validate_on_submit():
+        flash(f'School inputted: School name {form.school_name}, with {form.no_of_teams} number of teams.')
+        return redirect(url_for('index'))
+    return render_template("forms.html", title="School Input", form=form)
