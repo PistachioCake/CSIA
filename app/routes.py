2bf80822e5cbb6b6ac63ade6d0c1bbcd1de87d3e
PistachioCake <rushil.udani@gmail.com>
2020-02-10 22:51:01 -0600

    LARGE UPDATE: add an app/teams page and a form.
    Update app\routes.py to include a sample dummy competition, which it
    renders with the teams.html page, and a school form. It flashes the
    values of these fields on submit.
    Update base.html to add aesthetics - just a space under the navbar
    app\forms.py contains classes that outline each form. SchoolForm is used
    to create a new school with teams.
    app\templates\teams.html is the template for the teams page and
    currently just contains the form.

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,38 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import SchoolForm
+
+from collections import namedtuple
+
+Team = namedtuple('Team', ['name', 'password'])
+comp1 = {
+    "Westwood":[
+        Team("Westwood #1", "never"),
+        Team("Westwood #2", "gonna"),
+        Team("Westwood #3", "give-"),
+        Team("Westwood #4", "youup"),
+    ],
+    "West Lake":[
+        Team("West Lake #1", "never"),
+        Team("West Lake #2", "gonna"),
+        Team("West Lake #3", "let--"),
+        Team("West Lake #4", "you--"),
+        Team("West Lake #5", "down-"),
+    ],
+}
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html", title="Home")
 
-@app.route('/teams')
+@app.route('/teams', methods=['GET', 'POST'])
 def teams():
-    return render_template("teams.html", title="Teams", page_name="Teams")
+    form = SchoolForm()
+    if form.validate_on_submit():
+        flash(f'School inputted: School name {form.school_name.data}, with {form.no_of_teams.data} number of teams.')
+        return redirect(url_for('index'))
+    return render_template("teams.html", title="Teams", competition=comp1, form=form)
 
 @app.route('/admins')
 def admins():
