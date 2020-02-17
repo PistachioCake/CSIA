8d4bc108dad66e7e0c586240dc43bfd404dcebb1
PistachioCake <rushil.udani@gmail.com>
2020-02-17 12:37:02 -0600

    Prepare for using the database with teams.html
    Copy the sample data over to app.db
    Rename some of the fields in app/models.py to make it more similar to
    the sample data structure. Notably, display_name -> name. Also, add a
    password field to the team class.
    Clean up app/templates/teams.html by using some macros to make
    everything easier to see.

--- app/models.py
+++ app/models.py
@@ -1,31 +1,32 @@
 from app import db
 
-# should display_name be more specific to the class itself?
-# ie. Competition.display_name -> Competition.competition_name
+# should name be more specific to the class itself?
+# ie. Competition.name -> Competition.competition_name
 
 class Competition(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    display_name = db.Column(db.String(64), index=True, unique=True)
+    name = db.Column(db.String(64), index=True, unique=True)
     schools = db.relationship('School', backref='competition name', lazy='dynamic')
 
     def __repr__(self):
-        return f"<Competition {self.display_name} with x schools>"
+        return f"<Competition {self.name} with {len(self.schools.all())} schools>"
 
 class School(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    display_name = db.Column(db.String(64), index=True, unique=True)
+    name = db.Column(db.String(64), index=True, unique=True)
     teams = db.relationship('Team', backref='school name', lazy='dynamic')
     competition_id = db.Column(db.Integer, db.ForeignKey('competition.id'))
 
     def __repr__(self):
-        return f"<School {self.display_name} with x teams>"
+        return f"<School {self.name} with {len(self.teams.all())} teams>"
     
 class Team(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    display_name = db.Column(db.String(64), unique=True) # TODO?: Does this need a unique=True or a index=True?
+    name = db.Column(db.String(64), unique=True) # TODO?: Does this need a unique=True or a index=True?
+    password = db.Column(db.String(64))
     team_num = db.Column(db.Integer, index=True)
     school_id = db.Column(db.Integer, db.ForeignKey('school.id'))
 
     def __repr__(self):
-        return f"<Team {self.display_name} from school {school_id}>"
+        return f"<Team {self.name} with password {self.password}>"
     
