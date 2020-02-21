e95e0ed3775d5e4091154fa42a5057e92b294de5
PistachioCake <rushil.udani@gmail.com>
2020-02-21 15:49:19 -0600

    Move all databse requests into actions.py
    app/routes.py: remove database requests.
    app/actions.py: created and contains all databse actions.

---
+++ app/actions.py
@@ -0,0 +1,37 @@
+from app import db
+import app.models as models
+
+def get_competition(**kwargs):
+    return models.Competition.query.filter_by(**kwargs).first()
+
+def get_or_make_competition(**kwargs):
+    comp = models.Competition.query.filter_by(**kwargs).first()
+    if not comp:
+        comp = models.Competition(**kwargs)
+        db.session.add(comp)
+        db.session.commit()
+    return comp
+
+def get_team(**kwargs):
+    return models.Team.query.filter_by(**kwargs).first()
+
+def change_team_password(team, new_password):
+    team.password = new_password
+    db.session.commit()
+
+def remove_team(team):
+    db.session.delete(team)
+    db.session.commit()
+
+def add_school(competition, name, no_of_teams):
+    # Create a school associated with the competition
+    school = models.School(name=name, competition=competition)
+    # Add this school to the database
+    db.session.add(school)
+    for i in range(no_of_teams):
+        # Create a team associated with the school
+        team = models.Team(name=f"{name} #{i}", password="foobar", school=school)
+        # Add this team to the database
+        db.session.add(team)
+    # Commit data to persistent storage
+    db.session.commit()
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,7 @@
 from flask import render_template, flash, redirect, url_for
-from app import app, db
+from app import app
 from app.forms import SchoolForm, EditTeamForm
-import app.models as models
-
+from app import actions
 
 @app.route('/')
 @app.route('/index')
@@ -11,45 +10,29 @@ def index():
 
 @app.route('/teams', methods=['GET', 'POST'])
 def teams():
-    comp1 = models.Competition.query.filter_by(name="comp1").first()
-    if not comp1:
-        comp1 = models.Competition(name="comp1")
-        db.session.add(comp1)
-        db.session.commit()
+    comp1 = actions.get_or_make_competition(name="comp1")
 
     # For the school form:
     add_school_form = SchoolForm()
     if add_school_form.submit_school.data and add_school_form.validate_on_submit():
         flash(f'School inputted: School name {add_school_form.school_name.data}, with {add_school_form.no_of_teams.data} teams.')
-        # Create a school associated with the competition
-        school = models.School(name=add_school_form.school_name.data, competition=comp1)
-        # Add this school to the database
-        db.session.add(school)
-        for i in range(add_school_form.no_of_teams.data):
-            # Create a team associated with the school
-            team = models.Team(name=f"{add_school_form.school_name.data} #{i}", password="foobar", school=school)
-            # Add this team to the database
-            db.session.add(team)
-        # Commit data to persistent storage
-        db.session.commit()
+        actions.add_school(comp1, add_school_form.school_name.data, add_school_form.no_of_teams.data)
     
     # For the team form
     edit_team_form = EditTeamForm()
     if edit_team_form.submit_password.data and edit_team_form.is_submitted():
         if edit_team_form.validate():
-            team = models.Team.query.get(edit_team_form.team_id.data)
+            team = actions.get_team(id=edit_team_form.team_id.data)
             flash(f'Team {team.name} updated: Password changed from {team.password} to {edit_team_form.password.data}')
-            team.password = edit_team_form.password.data
-            db.session.commit()
+            actions.change_team_password(team, edit_team_form.password.data)
         else:
             flash(f'Unable to update team.')
             flash(f'{edit_team_form.errors}')
 
     if edit_team_form.submit_delete.data and edit_team_form.is_submitted():
-            team = models.Team.query.get(edit_team_form.team_id.data)
+            team = actions.get_team(id=edit_team_form.team_id.data)
             flash(f'Team {team.name} deleted.')
-            db.session.delete(team)
-            db.session.commit()
+            actions.remove_team(team)
 
     return render_template("teams.html", title="Teams", competition=comp1, add_school_form=add_school_form, edit_team_form=edit_team_form)
 
